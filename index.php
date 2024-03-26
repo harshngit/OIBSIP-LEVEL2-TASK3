a8899dcbe8241b58206284563b0ed8c00a76e424
Harsh Nikharge <158061768+harshngit@users.noreply.github.com>
2024-03-26 23:59:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Tribute Page_MS DHONI</title>
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+    <!-- cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11/lib/typed.min.js"></script>
+</head>
+<body>
+    <!-- header section starts  -->
+    <header class="navbar">
+        <div class="logo">
+            <h1>Ms.Dhoni <i class="fa-solid fa-baseball"></i></h1>
+        </div>
+        <div class="nav_links">
+            <nav>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#car">Career</a>
+                <a href="#ach">Achivement</a>
+            </nav>
+        </div>
+    </header>
+    <!-- header section ends  -->
+    <!-- home section starts -->
+    <section class="home" id="home">
+        <div class="content">
+            <h2 class="heading">My Tribute to</h2>
+            <h1 class="subheading">MS. Dhoni is a <span id="text"></span></h1>
+            <p class="info">MS Dhoni is a former <span>Indian Cricketer</span> renowed for his captaincy and finishing skills</p>
+            <a href="#about">About him  <i class="fa-solid fa-arrow-down"></i></a>
+        </div>
+        <div class="img">
+            <img src="img/bg.jpg" alt="ms dhoni image">
+        </div>
+        
+    </section>
+    <!-- home section ends -->
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <h1 class="title">About <span id="span"></span></h1>
+        <div class="about_sec">
+            <div class="img">
+                <img src="img/about.jpg" alt="about img">
+            </div>
+            <div class="about_content">
+                <div class="content">
+                <h1 class="name">Mahendra Singh Dhoni</h1>
+                <p>Mahendra Singh Dhoni (commonly known as M. S. Dhoni) is an Indian cricketer who captained 
+                    the Indian team in limited-overs formats from 2007 to 2016 and in Test cricket from 2008 to 2014. 
+                    An attacking right-handed middle-order batsman and wicket-keeper, he is widely regarded as one of the
+                     greatest finishers in limited-overs cricket.He made his One Day International (ODI) debut in December 
+                     2004 against Bangladesh, and played his first Test a year later against Sri Lanka.</p>
+                <p>Renowned for his cool temperament under pressure, Dhoni led India to several significant victories, including the 2007 ICC World Twenty20, 
+                    the 2010 and 2016 Asia Cups, the 2011 ICC Cricket World Cup, and the 2013 ICC Champions Trophy. He was also a highly effective 
+                    wicketkeeper-batsman, known for his lightning-quick stumpings and match-winning performances with the bat. Dhoni's impact on Indian
+                     cricket transcends his on-field achievements, making him one of the most iconic figures in the sport's history.</p>
+            </div>
+            </div>
+         </div>
+        <div class="table">
+            <div class="t1">
+                <!-- table 1 -->
+                <table class="t1">
+                    <thead>
+                        <tr>
+                            <th>Personal Information</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Name:</th>
+                            <td>Mahendra Singh Dhoni</td>
+                        </tr>
+                        <tr>
+                            <th>Born</th>
+                            <td>7 July 1981 (age 35) Ranchi, Bihar, India</td>
+                        </tr>
+                        <tr>
+                            <th>Nickname</th>
+                            <td>Mahi, MS, MSD, Captain Cool</td>
+                        </tr>
+                        <tr>
+                            <th>Height</th>
+                            <td>5 ft 9 in (1.75 m)</td>
+                        </tr>
+                        <tr>
+                            <th>Batting style</th>
+                            <td>Right-handed</td>
+                        </tr>
+                        <tr>
+                            <th>Role</th>
+                            <td>Wicket-keeper</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+                <!-- table 2 -->
+            <div class="t2">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Domestic team information</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Years</th>
+                        <th>Team</th>
+                    </tr>
+                    <tr>
+                        <td>1999/00 - 2003/04</td>
+                        <td>Bihar</td>
+                    </tr>
+                    <tr>
+                        <td>2004/05 </td>
+                        <td>Jharkhand</td>
+                    </tr>
+                    <tr>
+                        <td>2008 - 2015</td>
+                        <td>Chennai Super Kings</td>
+                    </tr>
+                    <tr>
+                        <td>2016 - present</td>
+                        <td>Rising Pune Supergiants</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+                <!--table 3 -->
+            <div class="t3">
+            <table>
+                <thead>
+                    <tr>
+                        <th>International Information</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>National Side</th>
+                        <td>India</td>
+                    </tr>
+                    <tr>
+                        <th>Test debut</th>
+                        <td>2 December 2005 v Sri Lanka</td>
+                    </tr>
+                    <tr>
+                        <th>last test</th>
+                        <td>	26 December 2014 v Australia</td>
+                    </tr>
+                    <tr>
+                        <th>ODI shirt no.</th>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <th>T20I debut</th>
+                        <td>1 December 2006 v South Africa</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <!-- about section ends -->
+
+    <!-- career section start -->
+    <section id="car" class="career">
+    <h1 class="title">Career :- <span id="span1"></span></h1>
+    <div class="timeline">
+        <h1 class="head">Mahendra Singh Dhoni: <span>A timeline</span></h1>
+        <div class="content">
+            <ul>
+            <li><strong>December 23, 2004:</strong> Makes his ODI debut against Bangladesh in Chittagong. Is run out for 0 after facing one ball, in a match India win by 11 runs.</li>
+            <li><strong>April 5, 2005:</strong> The first ODI hundred by an Indian wicketkeeper. After scores of 0, 12, 7* and 3, Dhoni is sent up the order to No 3 against Pakistan in Visakhapatnam and proceeds to smash 148 off 123 balls with 15 fours and four sixes, setting the platform for India's 356/9. The world wakes up to Dhoni's potential.</li>
+            <li><strong>October 31, 2005:</strong> Dhoni stamps himself into the history books with a record 183* against Sri Lanka in Jaipur, helping India chase 301 with six wickets and 23 deliveries to spare. Batting at No 3, his 145-ball hundred makes a stiff target look well short and India go up 3-0 in the best-of-seven series.</li>
+            <li><strong>September 13, 2007:</strong> Dhoni's first international match in charge of India is a wash out against Scotland in their ICC World Twenty20 opener. Few could have predicted how India's tournament will pan out.</li>
+            <li><strong>September 24, 2007:</strong> India win the inaugural World Twenty20 by beating Pakistan in an absolute humdinger in Johannesburg. Dhoni is an overnight sensation once more and the landscape of T20 cricket is changed forever.</li>
+            <li><strong>October 2, 2007:</strong> In the next match in Kochi, Dhoni scores his first fifty as captain - 58 off 88 balls - as India are beaten by 84 runs to fall behind 0-1 in the seven-match series.</li>
+            <li><strong>November 15, 2007:</strong> First ODI series win for Dhoni as captain, as India beat Pakistan by six wickets in Gwalior to take the lead 3-1 in the best-of-five series. His contribution? An unbeaten 45 off 45 balls as 256 is chased with six wickets in hand.</li>
+            <li><strong>March 4, 2008:</strong> A momentous occasion for India as they win their first ODI series in Australia. The landmark comes in the second final of the CB Series, which India win by nine runs to make it 2-0.</li>
+            <li><strong>November 26, 2008:</strong> Dhoni's first 5-0 sweep of any team. Victory by six wickets in Cuttack whitewashes Kevin Pietersen's England.</li>
+            <li><strong>March 11, 2009:</strong> Dhoni becomes the first Indian captain to win an ODI series in New Zealand. A whirlwind century from Virender Sehwag seals a ten-wicket win in Hamilton.</li>
+            <li><strong>February 24, 2010:</strong> India win a three-match series at home over South Africa, riding on Sachin Tendulkar's record-setting 200*. Dhoni injects 68 off 35 balls to a total of 401/3.</li>
+            <li><strong>June 24, 2010:</strong> India lift the Asia Cup under Dhoni, beating Sri Lanka by 81 runs in Dambulla.</li>
+            <li><strong>April 2, 2011:</strong> India win the World Cup after 28 years, with the skipper hitting the winning runs with a six. Promoting himself up the order, Dhoni shrugs off his poor run in the tournament with the innings of his life: 91* off 79 balls.</li>
+            <li><strong>October 25, 2011:</strong> India salvage some pride with a 5-0 win over England when Alastair Cook's team visit. Dhoni is Player of the Series.</li>
+            <li><strong>June 23, 2013:</strong> Dhoni marshals his troops superbly to lead India to a narrow five-run win in the final of the Champions Trophy over hosts England, thus becoming the first captain to hold all three of the ICC's major titles.</li>
+            <li><strong>September 2, 2014:</strong> A sliver of brightness for India away from home. They beat England by nine wickets in Birmingham to win the ODI leg of another difficult tour.</li>
+            <li><strong>March 23, 2016:</strong> Dhoni's coolness and ability to back players under immense pressure is to the fore in Bangalore, where Bangladesh are beaten by one run during the World Twenty20. Dhoni helps India pull off an incredible heist.</li>
+            <li><strong>January 4, 2017:</strong> Announces that he is giving up the captaincy, but remains available for selection for the upcoming England series.</li>
+            </ul>
+        </div>
+    </div>
+    </section>
+    <!-- career section ends -->
+
+    <!-- Achivements section start -->
+    <section class="achiv" id="ach">
+        <h1 class="title">Achivements :- <span id="span2"></span></h1>
+        <div class="container">
+            <div class="row">
+                <div class="card">
+                <img src="img/2011.jpeg" alt="">
+                <div class="content"><h2 class="heading">2011 ICC Cricket World Cup Victory</h2>
+                <p>Dhoni led the Indian cricket team to victory in the 2011 ICC Cricket World Cup, 
+                    held in India. He played a crucial role in the final match against Sri Lanka by 
+                    scoring an unbeaten 91 runs and sealing the victory with a six, thereby ending 
+                    India's 28-year wait for a World Cup title.</p></div>
+                </div>
+                <div class="card">
+                    <img src="img/2007.jpg" alt="">
+                    <div class="content">
+                    <h2 class="heading">2007 ICC World Twenty20 Championship</h2>
+                    <p>Under Dhoni's captaincy, India won the inaugural ICC World Twenty20 Championship held in South Africa. 
+                        His strategic leadership and calm demeanor were instrumental in guiding the team to victory in this 
+                        fast-paced format.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/2013.jpg" alt="">
+                    <div class="content">
+                    <h2 class="heading">2013 ICC Champions Trophy Triumph</h2>
+                    <p>Dhoni captained India to victory in the 2013 ICC Champions Trophy held in England. 
+                        India defeated England in a rain-affected final to clinch the title, with Dhoni's 
+                        tactical acumen shining through in crucial moments.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/2008.jpg" alt="">
+                    <div class="content">
+                    <h2 class="heading">2007-08 CB Series Victory in Australia</h2>
+                    <p>Dhoni led India to a historic victory in the 2007-08 Commonwealth Bank Series in Australia, 
+                        which included Australia and Sri Lanka. This series win marked India's first-ever triumph 
+                        in a bilateral ODI series in Australia.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/test.jpg" alt="">
+                    <div class="content">
+                    <h2 class="heading">Number 1 Test Ranking</h2>
+                    <p> Under Dhoni's leadership, the Indian cricket team achieved the number one ranking in Test cricket for the 
+                        first time in December 2009. This was a significant milestone in Indian cricket history, showcasing Dhoni's
+                        ability to lead the team across all formats.</p>
+                    </div>
+                   </div>
+                   <div class="card">
+                    <img src="img/2016.jpg" alt="">
+                    <div class="content">
+                    <h2 class="heading">2016 Asia Cup Victory</h2>
+                    <p> Dhoni led the Indian cricket team to victory in the 2016 Asia Cup, held in Bangladesh. Under his captaincy,
+                        India displayed excellent teamwork and skill to clinch the title, further solidifying Dhoni's legacy as a 
+                        successful leader.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Achivements section ends -->
+
+
+    <!-- footer section start -->
+    <footer>
+        <h1 class="footer_text"> Design By @codewitharsh</h1>
+        <img src="img/logo.jpg" id="logo_footer">
+    </footer>
+    <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+    <!-- script js -->
+    <script>
+        // text animation effect for heading
+        var typed = new Typed('#text', {
+        strings: ["Captain cool", "Mahi", "Thala"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+        });
+        var typed = new Typed('#span', {
+        strings: ["Him"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+        });
+        var typed = new Typed('#span1', {
+        strings: ["Timeline"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+        });
+        var typed = new Typed('#span2', {
+        strings: ["Successes"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+        });
+    </script>
+</body>
+</html>
